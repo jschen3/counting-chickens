11f01c8a687ad26548872d56e415ef20f696673d
Jimmy Chen <jimmy@jimmys-mbp.lan>
2021-12-30 16:16:40 -0700

    basic card material react

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './App'
+import App from './App'
 import './styles/index.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.querySelector('#app'));
+
