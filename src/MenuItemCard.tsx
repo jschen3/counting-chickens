40ed92b301c6f14c8f480b03a35f19158cff9dcc
Jimmy Chen <jimmy@jimmys-mbp.lan>
2021-12-30 22:12:13 -0700

    need to use state in material and react to make render work correct

--- src/MenuItemCard.tsx
+++ src/MenuItemCard.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as dotenv from 'dotenv';
+import {Buffer} from 'buffer';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -7,10 +8,11 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import AWS from 'aws-sdk';
 async function getImage(imagePath: string){
-  const AWS_ACCESS_KEY_ID = 'AKIA5V2SVVCIY3G5YQ7S';
-  const AWS_SECRET_ACCESS_KEY = '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';
-  const AWS_S3_BUCKET = 'counting-chickens-menu-items';
-  const AWS_S3_REGION = 'us-east-2';
+  dotenv.config()
+  const AWS_ACCESS_KEY_ID = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
+  const AWS_SECRET_ACCESS_KEY = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+  const AWS_S3_BUCKET = process.env.REACT_APP_AWS_S3_BUCKET;
+  const AWS_S3_REGION = process.env.REACT_APP_AWS_S3_REGION;
   AWS.config.update({
     region: AWS_S3_REGION,
     accessKeyId: AWS_ACCESS_KEY_ID,
@@ -27,13 +29,16 @@ async function getImage(imagePath: string){
        console.log(err, err.stack)
     }
     else{
-      const buf = Buffer.from(data);
-      const base64 = buf.toString('base64');
-      return base64;
+      return data;
    }}).promise();
   return image;
 }
 
+function encode(data){
+  const buf = Buffer.from(data);
+  const base64 = buf.toString('base64');
+  return base64
+}
 export default async function MenuItemCard() {
   const singleCardInfo={
     name: 'hot dog',
@@ -64,8 +69,10 @@ export default async function MenuItemCard() {
     visible: true
   };
   const image = await getImage(singleCardInfo.imagePath);
-  
+  const imageData = encode(image.Body)
+  //<!--{`data:image/png;base64, ${imageData}`} -->
 
+  //https://stackoverflow.com/questions/46879693/how-to-set-state-in-react-and-material-ui-over-js-files
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
@@ -74,7 +81,7 @@ export default async function MenuItemCard() {
       <CardMedia
         component="img"
         height="194"
-        image={image}
+        src="example.png" 
         alt="Paella dish"
       />
       <CardContent>
