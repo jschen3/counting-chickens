84cd0b505beabddc0d1216ff8495cad44f7867c0
Jimmy Chen <jimmy@jimmys-mbp.lan>
2021-12-30 19:11:38 -0700

    error with aws presigned

--- src/MenuItemCard.tsx
+++ src/MenuItemCard.tsx
@@ -1,116 +1,87 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import * as dotenv from 'dotenv';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
+import AWS from 'aws-sdk';
+async function getImage(imagePath: string){
+  const AWS_ACCESS_KEY_ID = 'AKIA5V2SVVCIY3G5YQ7S';
+  const AWS_SECRET_ACCESS_KEY = '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';
+  const AWS_S3_BUCKET = 'counting-chickens-menu-items';
+  const AWS_S3_REGION = 'us-east-2';
+  AWS.config.update({
+    region: AWS_S3_REGION,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  });
+  const s3 = new AWS.S3();
+  const params = {
+    Bucket: AWS_S3_BUCKET,
+    Key: imagePath,
+    ResponseContentEncoding:'image/png'
+  };
+  const image =  s3.getObject(params, function(err, data){
+    if (err){
+       console.log(err, err.stack)
+    }
+    else{
+      const buf = Buffer.from(data);
+      const base64 = buf.toString('base64');
+      return base64;
+   }}).promise();
+  return image;
 }
 
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-export default function MenuItemCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+export default async function MenuItemCard() {
+  const singleCardInfo={
+    name: 'hot dog',
+    imagePath: 'hot dog.png',
+    id: '3fa8',
+    description: 'hot dog',
+    price: 5.99,
+    ingredients: [
+      {
+        name: 'hot dog',
+        qty: 1,
+        unit: 'ea',
+        ingredientId: 12,
+      },
+      {
+        name: 'bread',
+        qty: 1,
+        unit: 'bun',
+        ingredientId: 9,
+      },
+    ],
+    itemType: 'entree',
+    category: 'sandwich',
+    notes: 'kids meal',
+    ranking: 6,
+    restaurantName: 'Sample Restaurant',
+    restaurantId: '6f0e',
+    visible: true
   };
+  const image = await getImage(singleCardInfo.imagePath);
+  
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={singleCardInfo.name}
       />
       <CardMedia
         component="img"
         height="194"
-        image="/static/images/cards/paella.jpg"
+        image={image}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+         {singleCardInfo.description}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing={true}>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse timeout="auto" unmountOnExit={true}>
-        <CardContent>
-          <Typography paragraph={true}>Method:</Typography>
-          <Typography paragraph={true}>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph={true}>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph={true}>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
